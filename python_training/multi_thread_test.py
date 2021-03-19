5bdb0a62756002ca2fca341858a79e3f2428e5ca
Vignesh <vigneshdhakshnamoorthy@gmail.com>
2021-03-20 00:01:20 +0530

    home laptop updated

---
+++ python_training/multi_thread_test.py
@@ -0,0 +1,21 @@
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+
+def multi_thread():
+    print('Started')
+    time.sleep(1)
+    return 'Ended'
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    result = [executor.submit(multi_thread) for _ in range(10)]
+
+    for f in concurrent.futures.as_completed(result):
+        print(f.result())
+
+end = time.perf_counter()
+
+print(f"Took {round(end - start, 2)} Second's to Complete")
