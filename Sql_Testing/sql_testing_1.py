8493b4160495a0d2c6840fbe82f8aceee05ea60f
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-03-22 15:31:42 +0530

    sql project edited

--- Sql_Testing/sql_testing_1.py
+++ Sql_Testing/sql_testing_1.py
@@ -7,17 +7,38 @@ table_name = 'dbo.f_agent_master'
 username = 'sa'
 password = 'manoj.111'
 conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';UID=' + username + ';PWD=' + password)
 cursor = conn.cursor()
 
 # cursor.execute(f'SELECT * FROM {database}.{table_name};')
 # for row in cursor:
 #     print(row)
 
-sql_query = pd.read_sql_query(f'SELECT * FROM {database}.{table_name};',conn)
+sql_query = pd.read_sql_query(f'SELECT * FROM {database}.{table_name};', conn)
 
-# data_frame = pd.DataFrame(sql_query)
-# print(data_frame)
+# print(sql_query)
 
-File_Loc = "../Sql_Testing/agentdata.xlsx"
-sql_query.to_excel(File_Loc)
+# save files
+# File_Loc = "../Sql_Testing/agentdata.xlsx"
+# sql_query.to_excel(File_Loc)
+
+# Columns
+#print(sql_query.columns)
+
+# AGENT_USERNAME
+#print(sql_query["AGENT_USERNAME"])
+
+# head
+#print(sql_query.head(6))
+
+# iloc
+# print(sql_query.iloc[1:6])
+# print(sql_query.iloc[3,3])
+
+# # loc filter
+# print(sql_query.loc[sql_query["AGENT_USERNAME"] == "Vigneshd@shoppingzoneindia.com"])
+# print(sql_query.loc[sql_query["AGENT_USERNAME"].str.contains('Vignesh')])
+# print(sql_query.loc[sql_query["AGENT_USERNAME"].str.contains('Vignesh') | sql_query["AGENT_USERNAME"].str.contains('vignesh')])
+
+#index_vignesh = sql_query.loc[sql_query["AGENT_USERNAME"] == "Vigneshd@shoppingzoneindia.com"].index.item()
+#print(sql_query.iloc[index_vignesh])
