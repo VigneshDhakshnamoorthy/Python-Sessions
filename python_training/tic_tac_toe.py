0cba485f5e697626a577a0e40d94190eb8f96dde
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-03-29 12:05:55 +0530

    tic toe

--- python_training/tic_tac_toe.py
+++ python_training/tic_tac_toe.py
@@ -4,19 +4,19 @@ input_user={ 1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: 
 def print_board():
     for x in input_user.keys():
         if 1 <= x <= 3:
-            print(input_user[x], "|| ", end="")
+            print(input_user[x], " | ", end="")
 
     print()
 
     for x in input_user.keys():
         if 4 <= x <= 6:
-            print(input_user[x], "|| ", end="")
+            print(input_user[x], " | ", end="")
 
     print()
 
     for x in input_user.keys():
         if 7 <= x <= 9:
-            print(input_user[x], "|| ", end="")
+            print(input_user[x], " | ", end="")
 
 
 def input_from_x():
@@ -26,10 +26,16 @@ def input_from_x():
         if input_user[x] == " ":
             input_user[x]="X"
             print_board()
-            input_from_o()
+            if not winner_found():
+                input_from_o()
+            else:
+                winner_declare()
 
         else:
             input_from_x()
+
+
+
 
     else:
         print("Done")
@@ -42,42 +48,62 @@ def input_from_o():
         if input_user[x] == " ":
             input_user[x]="O"
             print_board()
-            input_from_x()
+            if not winner_found():
+                input_from_x()
+            else:
+                winner_declare()
 
         else:
             input_from_o()
+
+
+
     else:
         print("Done")
 
 
-win_log={"X": False, "O": False}
+win_log={ "X": False, "O": False }
 
 
 def winner_find(wl):
     if input_user[1] == input_user[2] == input_user[3] == wl:
         win_log[wl]=True
-    if input_user[4] == input_user[5] == input_user[6] == wl:
+    elif input_user[4] == input_user[5] == input_user[6] == wl:
         win_log[wl]=True
-    if input_user[7] == input_user[8] == input_user[9] == wl:
+    elif input_user[7] == input_user[8] == input_user[9] == wl:
         win_log[wl]=True
-    if input_user[1] == input_user[4] == input_user[7] == wl:
+    elif input_user[1] == input_user[4] == input_user[7] == wl:
         win_log[wl]=True
-    if input_user[2] == input_user[5] == input_user[8] == wl:
+    elif input_user[2] == input_user[5] == input_user[8] == wl:
         win_log[wl]=True
-    if input_user[3] == input_user[6] == input_user[9] == wl:
+    elif input_user[3] == input_user[6] == input_user[9] == wl:
         win_log[wl]=True
-    if input_user[1] == input_user[5] == input_user[9] == wl:
+    elif input_user[1] == input_user[5] == input_user[9] == wl:
         win_log[wl]=True
-    if input_user[3] == input_user[5] == input_user[7] == wl:
+    elif input_user[3] == input_user[5] == input_user[7] == wl:
         win_log[wl]=True
+
+
+def winner_found():
+    winner_find("X")
+    winner_find("O")
+    if win_log["X"]:
+        return True
+    elif win_log["O"]:
+        return True
+    else:
+        return False
+
+
+def winner_declare():
+    winner_find("X")
+    winner_find("O")
+    if win_log["X"]:
+        print()
+        print("X is Winner")
+    if win_log["O"]:
+        print()
+        print("O is Winner")
 
 
 input_from_x()
-
-winner_find("X")
-winner_find("O")
-
-if win_log["X"]==True:
-    print("X is Winner")
-if win_log["O"]==True:
-    print("O is Winner")
