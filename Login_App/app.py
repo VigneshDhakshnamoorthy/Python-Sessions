d2cf18a5f88b074da0ab2cbb756fbee0fc024bca
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-07 15:15:47 +0530

    office changes

--- Login_App/app.py
+++ Login_App/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, request
 
-app=Flask(__name__)
-file_loc="../Login_App/datebase/user_details.txt"
-success=False
+app = Flask(__name__)
+file_loc = "../Login_App/datebase/user_details.txt"
+success = False
 
 
 @app.route("/", methods=("POST", "GET"))
@@ -10,27 +10,26 @@ def main_page():
     if request.method == 'POST':
         uname=request.form['uname']
         upass=request.form['upass']
-        print(uname, upass)
-        return login_old(uname, upass)
+        login_old(uname, upass)
+
+        if success:
+            return render_template("login_success.html", name=uname)
+        else:
+            return redirect("/")
 
     else:
         return render_template("login.html")
 
 
 def login_old(name, password):
-    success=False
+    global success
     with open(file_loc, 'r') as file:
         for i in file:
-            a, b=i.split(",")
-            b=b.strip()
+            a, b = i.split(",")
+            b = b.strip()
             if a == name and b == password:
                 success=True
                 break
-
-    if success == True:
-        return render_template("login_success.html", name=name)
-    else:
-        return redirect("/")
 
 
 if __name__ == "__main__":
