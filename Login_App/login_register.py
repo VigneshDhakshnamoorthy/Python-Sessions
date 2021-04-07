4d580662e51245eef562afd45c34ca53e38c4059
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-07 15:09:54 +0530

    office changes

---
+++ Login_App/login_register.py
@@ -0,0 +1,50 @@
+file_loc = "../Login_App/datebase/user_details.txt"
+
+
+def log_cond (type_log_reg):
+    if type_log_reg == "login":
+        print ("#### Enter Login Details ####")
+        name = input ("Enter Your Name :").lower ()
+        password = input ("Enter Password :").lower ()
+        login_old (name,password)
+    else:
+        print ("#### Enter Registration Details ####")
+        name = input ("Enter Your Name :").lower ()
+        password = input ("Enter Password :").lower ()
+        register_new (name,password)
+
+
+def type_log ():
+    type_log_reg = input ("Enter Login / Register :").lower ()
+    if type_log_reg != "login" and type_log_reg != "register":
+        type_log ()
+    else:
+        log_cond (type_log_reg)
+
+
+def login_old (name,password):
+    success = False
+    with open (file_loc,'r') as file:
+        for i in file:
+            a,b = i.split (",")
+            b = b.strip ()
+            if a == name and b == password:
+                success = True
+                break
+
+    if (success == True):
+        print (f'Logged in :  name - {name} and password - {password}')
+    else:
+        log_cond ("login")
+
+
+def register_new (name,password):
+    with open (file_loc,'a') as file:
+        file.write (f'\n{name},{password}')
+
+    print (f'Registered :  name - {name} and password - {password}')
+    login_old(name,password)
+
+
+if __name__ == "__main__":
+    type_log ()
--- Sql_Testing/app.py
+++ Sql_Testing/app.py
@@ -10,14 +10,14 @@ sql_query['SCHEME_NAME']=sql_query['SCHEME_NAME'].str.upper()
 @app.route('/', methods=("POST", "GET"))
 def database_page():
     return render_template("index.html", tables=[sql_query[['SCHEME_NAME', 'GRAND_TOTAL']].loc[sql_query['GRAND_TOTAL']
-                                                                                               > 599].sort_values(
+                                                                                               > 699].sort_values(
         by='GRAND_TOTAL', ascending=False).reset_index(
         drop=True).to_html(classes='table', index=True)])
 
 
 @app.route('/select', methods=("POST", "GET"))
 def select_page():
-    product_list = sql_query['SCHEME_NAME'].loc[sql_query['GRAND_TOTAL'] > 599].sort_values(ascending=True)
+    product_list = sql_query['SCHEME_NAME'].loc[sql_query['GRAND_TOTAL'] > 699].sort_values(ascending=True)
     return render_template("select.html", product_list=product_list)
 
 
