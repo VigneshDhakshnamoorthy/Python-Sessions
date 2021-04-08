1e6291925d021f42a458aeef3695c25632720432
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-08 16:06:20 +0530

    office changes

--- pandas_numpy/pandas_sample_1.py
+++ pandas_numpy/pandas_sample_1.py
@@ -1,26 +1,28 @@
 import pandas as pd
 import numpy as np
 
+page=pd.read_html("https://money.rediff.com/gainers/bse/daily/groupall")
 
-page=pd.read_html("https://money.rediff.com/gainers/bse/daily/groupm")
-
-
-page[0]['% Change'] = np.array([float(li.replace("+ ", "")) for li in page[0]['% Change']])
-
-print(page[0].head(6))
-print(page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind'])
-print(page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind']>9.53)
+page[0]['% Change']=np.array([float(li.replace("+ ", "")) for li in page[0]['% Change']])
+# File_Loc = "Files/rediff.xlsx"
+# page[0].to_excel(File_Loc, index=False)
 
 
-File_Loc = "Files/rediff.xlsx"
+np_columns = np.array(page[0].columns)
+np_company = np.array(page[0]['Company'])
+np_prev_close = np.array(page[0]['Prev Close (Rs)'])
+np_current_price = np.array(page[0]['Current Price (Rs)'])
+np_change = np.array(page[0]['% Change'])
 
-page[0].to_excel(File_Loc, index=False)
-np_value = np.array(page[0]['% Change'])
-print(np_value.sum())
-print(np_value.max())
-print(np_value.min())
-value = [val for val in page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind']][0]
-print("Value : ", value)
+# print(np_columns)
+# print(np_company)
+# print(np_prev_close)
+# print(np_current_price)
+# print(np_change)
 
-print(np_value)
+np_index = np_current_price.max()
+print(f'Company : {np_company[np.where(np_prev_close == np_prev_close.max())]}')
+print(f'Prev Close (Rs) : {np_prev_close[np.where(np_prev_close == np_prev_close.max())]}')
+print(f'Current Price (Rs) : {np_current_price[np.where(np_prev_close == np_prev_close.max())]}')
+print(f'% Change : {np_change[np.where(np_prev_close == np_prev_close.max())]}')
 
