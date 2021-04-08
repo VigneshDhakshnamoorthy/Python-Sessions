229ffa6005486768db802826027110f367f57c95
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-08 13:33:49 +0530

    office changes

--- pandas_numpy/pandas_sample_1.py
+++ pandas_numpy/pandas_sample_1.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-page = pd.read_html("https://money.rediff.com/gainers")
-print(page[0]['Company'].head(6))
+page=pd.read_html("https://money.rediff.com/gainers/bse/daily/groupm")
+
+page[0]['% Change'] = [float(li.replace("+ ", "")) for li in page[0]['% Change']]
+
+print(page[0].head(6))
+print(page[0]['% Change'].loc[page[0]['Company'] == 'Anuroop Packaging Lt'])
+
+File_Loc = "Files/rediff.xlsx"
+
+page[0].to_excel(File_Loc, index=False)
