c01a3abc28d23d40d56c4bbea243daf0aaba2c45
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-08 14:45:18 +0530

    office changes

--- pandas_numpy/pandas_sample_1.py
+++ pandas_numpy/pandas_sample_1.py
@@ -1,12 +1,26 @@
 import pandas as pd
+import numpy as np
+
 
 page=pd.read_html("https://money.rediff.com/gainers/bse/daily/groupm")
 
-page[0]['% Change'] = [float(li.replace("+ ", "")) for li in page[0]['% Change']]
+
+page[0]['% Change'] = np.array([float(li.replace("+ ", "")) for li in page[0]['% Change']])
 
 print(page[0].head(6))
-print(page[0]['% Change'].loc[page[0]['Company'] == 'Anuroop Packaging Lt'])
+print(page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind'])
+print(page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind']>9.53)
+
 
 File_Loc = "Files/rediff.xlsx"
 
 page[0].to_excel(File_Loc, index=False)
+np_value = np.array(page[0]['% Change'])
+print(np_value.sum())
+print(np_value.max())
+print(np_value.min())
+value = [val for val in page[0]['% Change'].loc[page[0]['Company'] == 'ANG Lifesciences Ind']][0]
+print("Value : ", value)
+
+print(np_value)
+
