94287351fba697530a29afad2554da31a5f40ad9
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-10 16:37:54 +0530

    office changes

--- python_training/graph_pandas.py
+++ python_training/graph_pandas.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-y = [700, 1000, 600, 800, 600, 750, 800, 2500, 1600]
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [700, 1000, 600, 800, 750, 800, 2500, 1600]
+x = [1, 2, 3, 4, 5, 6, 7, 8]
 graph = pd.DataFrame (y,x)
 graph.plot (kind='line')
 plt.show()
