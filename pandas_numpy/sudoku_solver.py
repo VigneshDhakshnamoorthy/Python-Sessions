3ac035dc7f588d1f1df2ed066663e1bc3d0441e9
Vignesh <vigneshdhakshnamoorthy@gmail.com>
2021-04-13 22:14:46 +0530

    sudoku

--- pandas_numpy/sudoku_solver.py
+++ pandas_numpy/sudoku_solver.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-board = np.array([[3, 0, 6, 5, 0, 8, 4, 0, 0],
-                [5, 2, 0, 0, 0, 0, 0, 0, 0],
-                [0, 8, 7, 0, 0, 0, 0, 3, 1],
-                 [0, 0, 3, 0, 1, 0, 0, 8, 0],
-                 [9, 0, 0, 8, 6, 3, 0, 0, 5],
-                 [0, 5, 0, 0, 9, 0, 6, 0, 0],
-                 [1, 3, 0, 0, 0, 0, 2, 5, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 7, 4],
-                 [0, 0, 5, 2, 0, 6, 3, 0, 0]])
+board = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+         [5, 2, 0, 0, 0, 0, 0, 0, 0],
+         [0, 8, 7, 0, 0, 0, 0, 3, 1],
+         [0, 0, 3, 0, 1, 0, 0, 8, 0],
+         [9, 0, 0, 8, 6, 3, 0, 0, 5],
+         [0, 5, 0, 0, 9, 0, 6, 0, 0],
+         [1, 3, 0, 0, 0, 0, 2, 5, 0],
+         [0, 0, 0, 0, 0, 0, 0, 7, 4],
+         [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
 
 
 
@@ -27,10 +28,38 @@ def print_board(bo):
 
 
 
-def solve_array():
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == 0:
-                print(board[i][j],end=" ")
-        print()
-solve_array()
+def possible(row,column,number):
+    global board
+    for i in range(0,9):
+        if board[row][i] == number:
+            return False    
+    for j in range(0,9):
+        if board[j][column] == number:
+            return False
+    row0 = (row//3)*3
+    column0 = (column//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if board[row0+i][column0+j] == number:
+                return False
+    return True 
+           
+def solve():    
+    global board
+    for row in range(0,9):
+        for column in range(0,9):
+            if board[row][column] == 0:
+                for number in range(1, 10):
+                    if possible(row, column, number):
+                        board[row][column] = number
+                        solve()
+                        board[row][column] = 0
+                return
+    print_board(board)
+
+solve()
+
+
+
+
+
