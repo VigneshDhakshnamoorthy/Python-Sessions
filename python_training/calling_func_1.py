ee7a1481db0c337d292010caa92beea94f0b98bb
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-15 12:57:52 +0530

    local module added

--- python_training/calling_func_1.py
+++ python_training/calling_func_1.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = None
 
 
 def driver_open():
-    global driver
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    return driver
--- python_training/calling_func_2.py
+++ python_training/calling_func_2.py
@@ -1,9 +1,13 @@
-from python_training.calling_func_1 import *
+import sys
+sys.path.append(".")
 
-driver_open()
+from python_training.calling_func_1 import driver_open
 
+driver = driver_open()
 
 def driver_open2():
-    URL_link = "http://demo.guru99.com/test/newtours/register.php"
-    driver.get(URL_link)
+    url_link = "http://demo.guru99.com/test/newtours/register.php"
+    driver.get(url_link)
+
+
 driver_open2()
