4d89154809261c7aef7899d47fd98f6542c217be
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-15 11:08:32 +0530

    recursion

---
+++ python_training/recursion_func.py
@@ -0,0 +1,18 @@
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return (n*fact(n-1))
+
+
+print(f'factorial of 5 is {fact(5)}')
+
+
+def reverse_string(string: str) -> str:
+    if string == "":
+        return ""
+    else:
+        return string[-1] + reverse_string(string[:-1])
+
+
+print(f'reverse_string of stressed = {reverse_string("stressed")}')
