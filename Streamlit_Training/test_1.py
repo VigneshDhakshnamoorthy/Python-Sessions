578e6e8a84207e2585f1e5e1c53c70008f448355
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-04-16 16:04:38 +0530

    streamlit app

--- Streamlit_Training/test_1.py
+++ Streamlit_Training/test_1.py
@@ -1,2 +1,17 @@
 import streamlit as st
-st.write("Hello Vignesh")
+
+value = st.sidebar.selectbox("Box",("HI","Hello"))
+
+st.write(f"# {value} Vignesh")
+value2 = st.sidebar.selectbox("Box2", ("HI", "Hello"))
+
+file = st.file_uploader("Pick a File")
+date = st.date_input("Date")
+date_split = str(date).split("-")
+st.write(int(date_split[2]), int(date_split[1]), int(date_split[0]))
+
+with st.beta_container():
+    st.write("This is inside the container")
+    st.write("This is inside the container")
+    st.write("This is inside the container")
+st.write("This is outside the container")
