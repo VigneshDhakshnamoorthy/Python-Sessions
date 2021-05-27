25d505ffd009b7595c7fff334c4c45b6b4096604
Vignesh <vigneshdhakshnamoorthy@gmail.com>
2021-05-27 17:57:42 +0530

    area of object

--- python_training/list_tuple_array.py
+++ python_training/list_tuple_array.py
@@ -33,29 +33,29 @@ print(a)
 
 
 
-# print(list[2:4])
-# list[1] = "grapes"
-# print(list[1])
+print(list[2:4])
+list[1] = "grapes"
+print(list[1])
 
-# print("----- Tuple -----")
-# tuple = ("orange", "apple", "banana")
-# print(tuple)
+print("----- Tuple -----")
+tuple = ("orange", "apple", "banana")
+print(tuple)
 
-# print(tuple[2])
+print(tuple[2])
 
-# print(tuple[0:2])
+print(tuple[0:2])
 
-# print("----- Array -----")
+print("----- Array -----")
 
-# a = arr.array('i', [1, 2, 3])
-# print("The new created array is : ", end=" ")
-# for i in range(0, 3):
-#     print(a[i], end=" ")
-# print()
+a = arr.array('i', [1, 2, 3])
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
 
 
-# b = arr.array('d', [2.5, 3.2, 3.3])
-# print("The new created array is : ", end=" ")
-# for i in range(0, 3):
-#     print (b[i], end=" ")
+b = arr.array('d', [2.5, 3.2, 3.3])
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print (b[i], end=" ")
 
