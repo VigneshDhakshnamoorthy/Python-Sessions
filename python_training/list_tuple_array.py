42664faa8872386df14bf721b8058611a8bd2d71
Vignesh.D <vigneshdhakshnamoorthy@gmail.com>
2021-07-02 12:01:07 +0530

    office

--- python_training/list_tuple_array.py
+++ python_training/list_tuple_array.py
@@ -1,61 +1,15 @@
-
-import array as arr
-
-print("----- List -----")
-list = ["mango", "strawberry", "orange",
-        "apple"]
-
-print(list)
-list.append("Banana")
-print(list)
-list.remove("apple")
-print(list)
-list.insert(1,"gowa")
-print(list)
-list.sort()
-print(list)
-print(f'index of strawberry = {list.index("strawberry")}')
-list[list.index("strawberry")] = "grapes"
-print(list)
-
-a = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1]
-
-replacements = {
-    1: 10,
-    2: 20,
-    3: 30,
-}
-print(replacements.get(4,400))
-
-a = [replacements.get(x, x) for x in a]
-
-print(a)
-
-
-
-# print(list[2:4])
-# list[1] = "grapes"
-# print(list[1])
-
-# print("----- Tuple -----")
-# tuple = ("orange", "apple", "banana")
-# print(tuple)
-
-# print(tuple[2])
-
-# print(tuple[0:2])
-
-# print("----- Array -----")
-
-# a = arr.array('i', [1, 2, 3])
-# print("The new created array is : ", end=" ")
-# for i in range(0, 3):
-#     print(a[i], end=" ")
-# print()
-
-
-# b = arr.array('d', [2.5, 3.2, 3.3])
-# print("The new created array is : ", end=" ")
-# for i in range(0, 3):
-#     print (b[i], end=" ")
-
+l = [1,2,3,4,5,5,6,7,33]
+s = (1,2,3,4,5)
+d = {1,2,3,4,5}
+print(l)
+l.append(6)
+l.pop(4)
+l.remove(33)
+print(l)
+ct = set(l)
+print(ct)
+for c in ct:
+    print(c,"=",l.count(c))
+ct.add(8)
+ct.remove(2)
+print(ct)
